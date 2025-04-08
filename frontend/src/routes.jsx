dbb49ec9d4ef5dc1c89525dda68757eeaa5e083f
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-04-08 16:58:22 -0300

    feat: adiciona tela de dashboard com listagem de pedidos pendentes, aceitos e finalizados

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import RegisterPage from './pages/Register';
+import DashboardCliente from './pages/DashboardCliente';
+
 
 
 export default function AppRoutes() {
@@ -9,6 +11,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<RegisterPage />} />
+        <Route path="/dashboard-cliente" element={<DashboardCliente />} />
       </Routes>
     </BrowserRouter>
   );

e77e8f9c7df8d9163513acb8dd2579c0077dfd68
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-04-12 13:51:48 -0300

    feat: Adição das rotas do Admin para integração com a API

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const adminAuthRoutes = require('./src/routes/adminAuthRoutes');
 const userRoutes = require('./src/routes/userRoutes');
 const discardRequestRoutes = require('./src/routes/discardRequestRoutes');
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+app.use('/admin', adminAuthRoutes);
 app.use('/users', userRoutes);
 app.use('/discard-requests', discardRequestRoutes);
 

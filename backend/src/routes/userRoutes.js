431c1dc4295a9f7ffdba94658e2234f816bec925
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-05-07 18:54:34 -0300

    refactor: remove a rota de login

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 
 router.post('/register', UserController.register);
-router.post('/login', UserController.login);
 router.get('/', UserController.index); 
 
 module.exports = router;

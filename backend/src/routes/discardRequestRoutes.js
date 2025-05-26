f8d091e03a06b7e08f82e6a58778baa7c7c66487
Eduardo Araujo <eduardoaraujo.contato13@gmail.com>
2025-05-25 21:09:32 -0300

    feat: adicionar rota para retornar pedidos por empresa

--- backend/src/routes/discardRequestRoutes.js
+++ backend/src/routes/discardRequestRoutes.js
@@ -9,5 +9,6 @@ router.put('/:id', DiscardRequestController.update);
 router.delete('/:id', DiscardRequestController.delete);
 router.get('/pendentes', DiscardRequestController.findPendentes);
 router.get('/empresa/:id/aceitos', DiscardRequestController.findAceitosByEmpresa);
+router.get('/empresa/:id/todos', DiscardRequestController.findAllByEmpresa);
 
 module.exports = router;
